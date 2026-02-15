2a57f19af9877a72b8512af065d4db4e022202a7
Naveen <naveensinghnegiwork@gmail.com>
2026-02-15 09:23:40 +0530

    Updating all branches (#4)
    * Delete DESCRIPTION.md
    
    * Create README.md with project details
    
    * UI feature (#1)
    
    * basic layout of dashboard and subject is created
    
    * basic layout of dashboard and subject is created

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,13 +3,13 @@ plugins {
 }
 
 android {
-    namespace = "com.example.academicvault"
+    namespace = "com.academicvault"
     compileSdk {
         version = release(36)
     }
 
     defaultConfig {
-        applicationId = "com.example.academicvault"
+        applicationId = "com.academicvault"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
@@ -38,6 +38,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.cardview)
+    implementation(libs.recyclerview)
+    implementation(libs.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ app/src/main/java/com/academicvault/MainActivity.java
@@ -0,0 +1,13 @@
+package com.academicvault;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
